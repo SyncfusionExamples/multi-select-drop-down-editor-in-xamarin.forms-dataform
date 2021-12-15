b8e239505879daa7cf63cab672064cd458484ade
SaiGanesh Sakthivel <saiganesh.sakthivel@syncfusion.com>
2021-12-15 14:46:49 +0530

    Sample added

---
+++ DataFormDropDown/DataFormDropDown/DataFormDropDown/Model/Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataFormDropDown
+{
+    public class Address : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String Name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+        public Address()
+        {
+
+        }
+
+        public string Name { get; set; }
+
+        private string country;
+        public string Country
+        {
+            get { return this.country; }
+            set
+            {
+                this.country = value;
+                this.RaisePropertyChanged("Country");
+            }
+        }
+    }
+}
